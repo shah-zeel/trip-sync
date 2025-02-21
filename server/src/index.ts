55a23ce1f66473494f05b734bbed1fa72464b357
Zeel Shah <zshah6@asu.edu>
2025-02-21 00:48:45 -0700

    Setup Fastify

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,9 +1,29 @@
+import fastify, { FastifyInstance } from "fastify";
 import { AppDataSource } from "./data-source";
-
+import config from "./config";
 class Application {
-  static async main() {
+  server: FastifyInstance;
+
+  constructor() {
+    this.server = fastify();
+  }
+
+  async main() {
     await AppDataSource.initialize();
+
+    this.server.get("/ping", async (request, reply) => {
+      return "pong\n";
+    });
+
+    this.server.listen({ port: config.port }, (err, address) => {
+      if (err) {
+        console.error(err);
+        process.exit(1);
+      }
+      console.log(`Server listening at ${address}`);
+    });
   }
 }
 
-export default Application;
+const appInstance = new Application();
+appInstance.main();
